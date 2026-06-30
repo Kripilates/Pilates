4042a10b96674a07a213c0e408ad83c80eaff15f
Kripilates <okristynka@gmail.com>
2026-06-30 08:33:11 +0200

    a

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='PB40-v29-circuit-rounds';
-const ASSETS=['./','index.html','manifest.json'];
+const CACHE='PB40-v30-day1-qa';
+const ASSETS=['./','index.html','manifest.json','style.css','app.js','data.js'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',e=>{
